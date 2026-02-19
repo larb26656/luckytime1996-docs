ac51de00408a7dbc3a1f3215bd5b9ee597a73d5f
Luckytime1996 <larb26656@gmail.com>
2026-02-19 12:26:41 +0700

    feat: ssl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,41 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-import tailwindcss from '@tailwindcss/vite';
-import starlightThemeRapide from 'starlight-theme-rapide';
+import tailwindcss from "@tailwindcss/vite";
+import starlightThemeRapide from "starlight-theme-rapide";
+import mermaid from "astro-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://docs.luckytime1996.dev",
   integrations: [
-      starlight({
-          title: 'Docs',
-          logo: {
-            src: '/public/favicon.png',
-          },
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/larb26656' }],
-          sidebar: [
-              {
-                  label: 'Cheetsheet',
-                  autogenerate: { directory: 'cheatsheet' },
-              },
-          ],
-          customCss: [
-            './src/styles/global.css',
-          ],
-          plugins: [starlightThemeRapide()],
-      }),
-	],
+    starlight({
+      title: "Docs",
+      logo: {
+        src: "/public/favicon.png",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/larb26656",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Cheetsheet",
+          autogenerate: { directory: "cheatsheet" },
+        },
+      ],
+      customCss: ["./src/styles/global.css"],
+      plugins: [starlightThemeRapide()],
+    }),
+    mermaid({
+      theme: "forest",
+      autoTheme: true,
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
